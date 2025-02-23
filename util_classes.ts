7e919fae87f45307146a262a4de00c7481a6387c
Skye <christiaanmens@gmail.com>
2025-02-23 20:52:00 +0100

    fix util class

--- util_classes.ts
+++ util_classes.ts
@@ -1,20 +1,22 @@
-import { i32leb128, Options, Strings, UnsizedType } from "./deps.ts";
+import { FixedLengthString, i32leb128, Options, UnsizedType } from "./deps.ts";
 
 export class VarIntString extends UnsizedType<string> {
-    constructor() {
-        super(1);
-    }
+  constructor() {
+    super(1);
+  }
 
-    readPacked(dt: DataView, options: Options = { byteOffset: 0}): string {
-        const len = i32leb128.readPacked(dt, options);
-        const strCodec = new Strings.FixedLength(len);
+  readPacked(dt: DataView, options: Options = { byteOffset: 0 }): string {
+    const len = i32leb128.readPacked(dt, options);
+    const strCodec = new FixedLengthString(len);
 
-        return strCodec.readPacked(dt, options);
-    }
+    return strCodec.readPacked(dt, options);
+  }
 
-    writePacked(value: string, dt: DataView, options: Options = { byteOffset: 0}): void {
-        i32leb128.writePacked(value.length, dt, options);
-        const strCodec = new Strings.FixedLength(value.length);
-        strCodec.writePacked(value, dt, options);
-    }
+  writePacked(value: string, dt: DataView, options: Options = { byteOffset: 0 }): void {
+    i32leb128.writePacked(value.length, dt, options);
+    const strCodec = new FixedLengthString(value.length);
+    strCodec.writePacked(value, dt, options);
+  }
 }
+
+export const varIntString: VarIntString = new VarIntString();
