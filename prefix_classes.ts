cf3c13a6983d0f71eca6d7762d532b9cf873db47
Skye <christiaanmens@gmail.com>
2025-05-08 21:45:33 +0200

    commit message to annoy a certain someone..

--- prefix_classes.ts
+++ prefix_classes.ts
@@ -1,35 +1,38 @@
-import { i32leb128, Options, UnsizedType } from "./deps.ts";
-
-const ENCODER = new TextEncoder();
-const DECODER = new TextDecoder();
+import { ArrayType, FixedLengthString, i32leb128, Options, UnsizedType } from "./deps.ts";
 
 export class PrefixedString extends UnsizedType<string> {
-  #prefix: UnsizedType<number>;
-  constructor(prefix: UnsizedType<number>) {
-    super(prefix.byteAlignment);
-    this.#prefix = prefix;
+  constructor(readonly prefixCodec: UnsizedType<number> = i32leb128) {
+    super(1);
   }
 
-  readPacked(dt: DataView, options: Options = { byteOffset: 0 }): string {
-    const len = this.#prefix.readPacked(dt, options);
-    const result = DECODER.decode(
-      new Uint8Array(dt.buffer, dt.byteOffset + options.byteOffset, len),
-    );
-
-    super.incrementOffset(options, len);
-
-    return result;
+  override readPacked(dt: DataView, options: Options = { byteOffset: 0 }): string {
+    const len = this.prefixCodec.readPacked(dt, options);
+    return new FixedLengthString(len).readPacked(dt, options);
   }
 
-  writePacked(value: string, dt: DataView, options: Options = { byteOffset: 0 }): void {
-    this.#prefix.writePacked(value.length, dt, options);
-    ENCODER.encodeInto(
-      value,
-      new Uint8Array(dt.buffer, dt.byteOffset + options.byteOffset, value.length),
-    );
-
-    super.incrementOffset(options, value.length);
+  override writePacked(value: string, dt: DataView, options: Options = { byteOffset: 0 }): void {
+    this.prefixCodec.writePacked(value.length, dt, options);
+    new FixedLengthString(value.length).writePacked(value, dt, options);
   }
 }
 
-export const varIntString = new PrefixedString(i32leb128);
+export class PrefixedArray<T> extends UnsizedType<T[]> {
+  constructor(
+    readonly type: UnsizedType<T>,
+    readonly prefixCodec: UnsizedType<number> = i32leb128,
+  ) {
+    super(type.byteAlignment);
+  }
+
+  override readPacked(dt: DataView, options: Options = { byteOffset: 0 }): T[] {
+    const len = this.prefixCodec.readPacked(dt, options);
+    return new ArrayType(this.type, len).readPacked(dt, options);
+  }
+
+  override writePacked(value: T[], dt: DataView, options: Options = { byteOffset: 0 }): void {
+    this.prefixCodec.writePacked(value.length, dt, options);
+    new ArrayType(this.type, value.length).writePacked(value, dt, options);
+  }
+}
+
+export const varIntString = new PrefixedString();
